a340ac2c9832e858e93f7ed79962d7d26b6d5c69
Chutirat Chaisan <donotdont1990@hotmail.com>
2018-09-10 15:34:04 +0000

    Add new wxMenu

--- wx/simple/communicate.cpp
+++ wx/simple/communicate.cpp
@@ -1,4 +1,5 @@
 #include "communicate.h"
+//#include "panels.h"
 #include "iostream"
 
 using namespace std;
@@ -6,6 +7,32 @@ using namespace std;
 Communicate::Communicate(const wxString& title)
        : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(640, 480))
 {
+  menubar = new wxMenuBar;
+  file = new wxMenu;
+  //file->Append(wxID_EXIT, wxT("&Quit"));
+  
+  file->Append(wxID_ANY, wxT("&New"));
+  file->Append(wxID_ANY, wxT("&Open"));
+  file->Append(wxID_ANY, wxT("&Save"));
+  file->AppendSeparator();
+
+  imp = new wxMenu;
+  imp->Append(wxID_ANY, wxT("Import newsfeed list..."));
+  imp->Append(wxID_ANY, wxT("Import bookmarks..."));
+  imp->Append(wxID_ANY, wxT("Import mail..."));
+
+  file->AppendSubMenu(imp, wxT("I&mport"));
+
+  quit = new wxMenuItem(file, wxID_EXIT, wxT("&Quit\tCtrl+W"));
+  file->Append(quit);
+  
+  
+  menubar->Append(file, wxT("&File"));  
+  SetMenuBar(menubar);
+
+  Connect(wxID_EXIT, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(Communicate::OnQuit));
+  Centre();
+
   panel = new wxPanel(this, wxID_ANY);
 
   SetIcon(wxIcon(wxT("ironman.ico")));
@@ -24,4 +51,9 @@ Communicate::Communicate(const wxString& title)
   panel->SetSizer(hbox);
 
   this->Centre();
+}
+
+void Communicate::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+  Close(true);
 }
